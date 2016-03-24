de3411fa224495cb5bd673f791bce4c09ffe7935
kobabasu <keiji.kobayashi.web@gmail.com>
2016-03-25 03:26:20 +0900

    各registerの追加と、空のタスクを追加

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -1,6 +1,43 @@
 'use strict';
 
 import gulp from 'gulp';
-import flux from './gulp/hello.es6';
+import shell from '/usr/local/lib/node_modules/gulp-shell';
 
-gulp.registry(flux);
+/**
+ * react
+ */
+import react from './gulp/react.es6';
+gulp.registry(react);
+
+/**
+ * sass
+ */
+import sass from './gulp/sass.es6';
+gulp.registry(sass);
+
+/**
+ * karma
+ */
+import karma from './gulp/karma.es6';
+gulp.registry(karma);
+
+/**
+ * e2e (protractor)
+ */
+import e2e from './gulp/e2e.es6';
+gulp.registry(e2e);
+
+
+/**
+ * gulp watch
+ */
+gulp.task('watch', shell.task([`
+  tput setaf 36 && echo '\n  gulp watch: empty task. \n'
+`]));
+
+/**
+ * gulp build
+ */
+gulp.task('build', shell.task([`
+  tput setaf 36 && echo '\n  gulp build: empty task. \n'
+`]));

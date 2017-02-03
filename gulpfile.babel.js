d9d1f625573a05e06a65b12c2cb2f98dfdaa5c57
kobabasu <keiji.kobayashi.web@gmail.com>
2017-02-03 21:30:46 +0900

    各パッケージのtaskを使用するよう変更

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -3,69 +3,28 @@
 import gulp from 'gulp';
 import shell from '/usr/local/lib/node_modules/gulp-shell';
 
-/**
- * for gulpfile.bable.js in parent directory
+/*
+ * import tasks
  */
-// import react from './gulp/tasks/react.es6';
-// import sass from './gulp/tasks/sass.es6';
-// import karma from './gulp/tasks/karma.es6';
-// import e2e from './gulp/tasks/e2e.es6';
+import react from './src/gulp/tasks/react.es6';
+import sass  from './sass/gulp/tasks/sass.es6';
+// import karma from './test/gulp/tasks/karma.es6';
+// import e2e   from './e2e/gulp/tasks/e2e.es6';
 
-/**
- * for gulpfile.bable.js in this directory
- */
-import react from './tasks/react.es6';
-import sass from './tasks/sass.es6';
-import karma from './tasks/karma.es6';
-import e2e from './tasks/e2e.es6';
-
-/**
- * react
- *
- * gulp src
- * gulp src:min
- * gulp src:watch
- * gulp src:docs
- * gulp src:build
- */
 gulp.registry(react);
-
-/**
- * sass
- *
- * gulp sass
- * gulp sass:min
- * gulp sass:watch
- * gulp sass:docs
- * gulp sass:build
- */
 gulp.registry(sass);
+// gulp.registry(karma);
+// gulp.registry(e2e);
 
-/**
- * karma
- *
- * gulp karma
+/*
+ * import admin tasks
  */
-gulp.registry(karma);
+// import admin_react from './admin/src/gulp/tasks/react.es6';
+// import admin_sass  from './admin/sass/gulp/tasks/sass.es6';
+// import admin_karma from './admin/test/gulp/tasks/karma.es6';
+// import admin_e2e   from './admin/e2e/gulp/tasks/e2e.es6';
 
-/**
- * e2e (protractor)
- *
- * gulp e2e
- */
-gulp.registry(e2e);
-
-
-/**
- * gulp watch
- */
-gulp.task('watch', shell.task([`
-  tput setaf 36 && echo '\n  gulp watch: empty task. \n'
-`]));
-
-/**
- * gulp build
- */
-gulp.task('build', shell.task([`
-  tput setaf 36 && echo '\n  gulp build: empty task. \n'
-`]));
+// gulp.registry(admin_react);
+// gulp.registry(admin_sass);
+// gulp.registry(admin_karma);
+// gulp.registry(admin_e2e);

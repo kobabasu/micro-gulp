c8b9c487b1ca517865e2885090c410b4fa98e65d
kobabasu <keiji.kobayashi.web@gmail.com>
2017-02-05 08:51:12 +0900

    コピペによるミスを修正

--- gulpfile.babel.js
+++ gulpfile.babel.js
@@ -10,15 +10,13 @@ import shell from '/usr/local/lib/node_modules/gulp-shell';
 // import sass    from './sass/gulp/tasks/sass.es6';
 // import karma   from './test/gulp/tasks/karma.es6';
 // import e2e     from './e2e/gulp/tasks/e2e.es6';
-// import specs  from './specs/gulp/tasks/e2e.es6';
-// import specs   from './specs/gulp/tasks/e2e.es6';
-// import schema  from './schema/gulp/tasks/e2e.es6';
+// import specs   from './specs/gulp/tasks/specs.es6';
+// import schema  from './schema/gulp/tasks/schema.es6';
 
 // gulp.registry(react);
 // gulp.registry(sass);
 // gulp.registry(karma);
 // gulp.registry(e2e);
-// gulp.registry(specs);
 // gulp.registry(specs);
 // gulp.registry(schema);
 
@@ -29,14 +27,12 @@ import shell from '/usr/local/lib/node_modules/gulp-shell';
 // import admin_sass    from './admin/sass/gulp/tasks/sass.es6';
 // import admin_karma   from './admin/test/gulp/tasks/karma.es6';
 // import admin_e2e     from './admin/e2e/gulp/tasks/e2e.es6';
-// import admin_specs  from './admin/specs/gulp/tasks/karma.es6';
-// import admin_specs   from './admin/specs/gulp/tasks/e2e.es6';
-// import admin_schema  from './admin/schema/gulp/tasks/e2e.es6';
+// import admin_specs   from './admin/specs/gulp/tasks/specs.es6';
+// import admin_schema  from './admin/schema/gulp/tasks/schema.es6';
 
 // gulp.registry(admin_react);
 // gulp.registry(admin_sass);
 // gulp.registry(admin_karma);
 // gulp.registry(admin_e2e);
-// gulp.registry(admin_specs);
 // gulp.registry(admin_specs);
 // gulp.registry(admin_schema);
